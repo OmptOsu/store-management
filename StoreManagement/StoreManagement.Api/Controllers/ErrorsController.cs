ebc56748e554133e07b862f2beef2e0af83d1251
Ompt <42302941+OmptOsu@users.noreply.github.com>
2024-12-20 20:46:03 -0300

    Error handling exemples implementation - Error Endpoint

---
+++ StoreManagement/StoreManagement.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreManagement.Api.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]//<--- Needed for swagger to work with routing error handling
+public class ErrorsController: ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error() => Problem();
+}
--- StoreManagement/StoreManagement.Api/Program.cs
+++ StoreManagement/StoreManagement.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using StoreManagement.Api.Errors;
 using StoreManagement.Api.Filters;
 using StoreManagement.Api.Middleware;
 using StoreManagement.Application;
@@ -9,7 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
+    /*------------------------------------------
+    //FILTER IMPLEMENTATION
     builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+    --------------------------------------------*/
+
+    builder.Services.AddControllers();
+
+    //------------------------------------------
+    //ERROR ENDPOINT IMPLEMENTATION
+    builder.Services.AddSingleton<ProblemDetailsFactory, StoreManagementProblemDetailsFactory>();
+    //------------------------------------------
+
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 }
@@ -26,6 +39,11 @@ var app = builder.Build();
     //MIDDLEWARE IMPLEMENTATION
     app.UseMiddleware<ErrorHandlingMiddleware>();
     --------------------------------------------*/
+
+    //------------------------------------------
+    //ERROR ENDPOINT IMPLEMENTATION
+    app.UseExceptionHandler("/error");
+    //------------------------------------------
 
     app.UseHttpsRedirection();
     app.MapControllers();
